da139402524c4360f85f226d887dfc20f3b34488
Maarten Hazewinkel <maarten@hazewinkel.org>
2026-08-24 22:25:17 +0200

    Test suggested solution from Joffrey Bion

--- test@macosArm64/dev/kwiksilver/mustache/KotestNativeTest.kt
+++ test@macosArm64/dev/kwiksilver/mustache/KotestNativeTest.kt
@@ -8,16 +8,16 @@ import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 
 // TODO see if this workaround becomes superfluous with newer versions of Kotlin Toolchain or Kotest.
-@OptIn(KotestInternal::class)
-class KotestNativeTest {
-    @Test
-    fun runKotestSpecs() = runBlocking {
-        invokeTestEngine(
-            listOf(
-                SpecRef.Function(::ParserTests, ParserTests::class),
-                SpecRef.Function(::MustacheSpecTests, MustacheSpecTests::class),
-            ),
-            null,
-        )
-    }
-}
+//@OptIn(KotestInternal::class)
+//class KotestNativeTest {
+//    @Test
+//    fun runKotestSpecs() = runBlocking {
+//        invokeTestEngine(
+//            listOf(
+//                SpecRef.Function(::ParserTests, ParserTests::class),
+//                SpecRef.Function(::MustacheSpecTests, MustacheSpecTests::class),
+//            ),
+//            null,
+//        )
+//    }
+//}
